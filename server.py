fb7c5868d676fd7e1f9115a7dd1ab0dfc51c3829
ElizavetaKasapen <40471098+ElizavetaKasapen@users.noreply.github.com>
2021-05-12 17:58:13 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
+import Pneumonia_detection_net
+from PIL import Image
+import io
+import base64
 
 app = Flask(__name__)
+net = Pneumonia_detection_net.nn_for_pneumonia_detection()
 
 @app.route('/')
 def index():
@@ -9,13 +14,24 @@ def index():
 # it works!
 @app.route('/upload_image/', methods=['POST'])
 def upload_image():
-  print ('I got clicked!')
+  file = None
+  prediction = None
   if request.method == 'POST':
-        # we will get the file from the request
-        file = request.files['upload_image']
-        # convert that to bytes
-        #img_bytes = file.read()
-  return '<h1>Click.</h1>'
+    if 'upload_image' not in request.files:
+      return render_template('index.html')
+    # we will get the file from the request
+    file = request.files['upload_image'] 
+    if file.filename =="":
+      return render_template('index.html')
+    # convert that to bytes
+    img_bytes = file.read()
+    image = Image.open(io.BytesIO(img_bytes))
+    image = image.convert("RGB")
+    image.save(image, "JPEG")
+    encoded_img_data = base64.b64encode(image.getvalue())
+    input_img = encoded_img_data.decode('utf-8')
+    prediction = net.to_predict(img_bytes)
+  return render_template('index.html', filename = file.filename, image = input_img, prob=prediction)
 
 if __name__ == '__main__':
   app.run(debug=True)
