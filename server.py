b9aa9390762f80af5c86c3cd92ae12d859513e20
ElizavetaKasapen <40471098+ElizavetaKasapen@users.noreply.github.com>
2021-05-13 22:47:15 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect, send_from_directory
 import Pneumonia_detection_net
 from PIL import Image
 import io
 import base64
+import os
 
 app = Flask(__name__)
 net = Pneumonia_detection_net.nn_for_pneumonia_detection()
+app.config["IMAGE_UPLOADS"] = "Uploads/"
 
 @app.route('/')
 def index():
@@ -24,14 +26,25 @@ def upload_image():
     if file.filename =="":
       return render_template('index.html')
     # convert that to bytes
-    img_bytes = file.read()
-    image = Image.open(io.BytesIO(img_bytes))
-    image = image.convert("RGB")
-    image.save(image, "JPEG")
-    encoded_img_data = base64.b64encode(image.getvalue())
-    input_img = encoded_img_data.decode('utf-8')
+    img_bytes = file.read()#for net
     prediction = net.to_predict(img_bytes)
-  return render_template('index.html', filename = file.filename, image = input_img, prob=prediction)
+
+    data = base64.b64encode(img_bytes)
+    data = data.decode()
+    img = '<img src="data:image/*;base64,{}">'.format(data)
+    #image = Image.open(file)
+    #image = image.convert("RGB")
+    #image = image.save(file.filename)
+    #encoded_img_data = base64.b64encode(image.getvalue())
+    #encoded_img_data = encoded_img_data.decode('utf-8')
+    ##encoded_img_data = base64.b64encode(image.getvalue())
+    #input_img = encoded_img_data.decode('utf-8')
+    file.save(os.path.join(app.config["IMAGE_UPLOADS"], file.filename))
+  return render_template('index.html', filename = file.filename, prob=prediction, image = '')
+
+@app.route('/uploads/<filename>')
+def send_uploaded_file(filename=''):
+    return send_from_directory(app.config["IMAGE_UPLOADS"], filename)
 
 if __name__ == '__main__':
   app.run(debug=True)
