4fa9a9da69635dee93d421097a607107ab93613a
ElizavetaKasapen <40471098+ElizavetaKasapen@users.noreply.github.com>
2021-05-30 15:08:41 +0300

    Add files via upload

--- Pneumonia_detection_net.py
+++ Pneumonia_detection_net.py
@@ -4,6 +4,7 @@ from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 import numpy as np
 import torch.nn.functional as func
+import shutil, os
 import data_processing
 
 class nn_for_pneumonia_detection():
@@ -28,10 +29,10 @@ class nn_for_pneumonia_detection():
         net = self.create_model()
         #print(net)
         loss = torch.nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(net.parameters(), lr=1.0e-3)
+        optimizer = torch.optim.Adam(net.parameters(), lr=1.0e-3) #mb net.fc.parameters(),
         # Decay LR by a factor of 0.1 every 7 epochs
         scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-        net = self.train_model(self.dataloaders, net, loss, optimizer, scheduler, num_epochs=1)
+        net = self.train_model(self.dataloaders, net, loss, optimizer, scheduler, num_epochs=40)
         self.writer.flush()
         self.writer.close()
         net.eval()
@@ -43,7 +44,9 @@ class nn_for_pneumonia_detection():
         x_batch, y_batch = next(iter(train_dataloader))
         #if you want to show model
         self.writer.add_graph(model, x_batch)
-
+         # initialize the early_stopping object
+        
+    
         #epoch_loss_dict =  dict.fromkeys(['train', 'val'], [])
     
         for epoch in range(num_epochs):
@@ -53,7 +56,7 @@ class nn_for_pneumonia_detection():
             for phase in ['train', 'val']:
                 if phase == 'train':
                     dataloader = train_dataloader
-                    #scheduler.step() # documentation said I have to do it after optim
+                    scheduler.step() # documentation said I have to do it after optim
                     model.train()  # Set model to training mode
                 else:
                     dataloader = val_dataloader
@@ -79,7 +82,7 @@ class nn_for_pneumonia_detection():
                         if phase == 'train':
                             loss_value.backward()
                             optimizer.step()
-                            scheduler.step()
+                            #scheduler.step()
                     # statistics
                     running_loss += loss_value.item()
                     running_acc += (preds_class == labels.data).float().mean()
@@ -93,6 +96,10 @@ class nn_for_pneumonia_detection():
                 #for epoch acc
                 self.writer.add_scalar("Acc/{}".format(phase), epoch_acc, epoch)
         #to see overfitting
+                self.writer.add_scalar("Lost/{}".format(phase), epoch, epoch_loss)
+                self.writer.add_scalar("Accuracy/{}".format(phase), epoch, epoch_acc)
+                # early_stopping needs the validation loss to check if it has decresed, 
+        # and if it has, it will make a checkpoint of the current model
                 #epoch_loss_dict[phase].append(epoch_loss) # mb. CHECK IT!
                 #if phase == 'train':
                    # epoch_loss_arr_train.append(epoch_loss)
@@ -105,19 +112,43 @@ class nn_for_pneumonia_detection():
         model = self.create_model()
         model.load_state_dict(torch.load(path))
         model.eval() 
-        #print(model.classifier)
+        print(model.classifier)
         return model
 
 #add path
     def to_predict(self, path):
         data_transform = data_processing.data_preprocessing_for_net()
         tensor = data_transform.transform_image_to_predict(path)
-        print(tensor)
         model = self.load_model()
-        #or maybe outputs = model.forward(tensor)
+        #outputs = model.forward(tensor)
         with torch.set_grad_enabled(False):
             outputs = model(tensor)
-            outputs = func.softmax(outputs, dim=1)
-        outputs = outputs.tolist()[0] #change. return dictionary class name - prob.
+            preds = outputs.argmax(dim=1)
+            outputs = func.softmax(outputs, dim=1) #or dim =0 
+        print(preds)
         return outputs
-        
+
+    def to_test(self):
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        data_transform = data_processing.data_preprocessing_for_net()
+        test_dataloader = data_transform.find_accuracy()
+        model = self.load_model()
+        running_acc = 0
+        for inputs, labels, paths in tqdm(test_dataloader):
+                    inputs = inputs.to(self.device)
+                    labels = labels.to(self.device)
+                    preds = model(inputs)
+                    preds_class = preds.argmax(dim=1)
+                    print(len(inputs))
+                    print(labels.data)
+                    print(preds_class)
+                    #COUNT LOSS!!!!!!!!!!!!!!!!!!!!!!! and add this function to test part in docs
+                    running_acc += (preds_class == labels.data).float().mean()
+                    for i in range(23):
+                        if(preds_class[i]==labels.data[i]):
+                            filename = os.path.splitext(os.path.basename(paths[i]))
+                            shutil.copyfile(paths[i],"test/"+filename[0]+filename[1], follow_symlinks=True) 
+        return (running_acc / len(test_dataloader))
+
+
+
