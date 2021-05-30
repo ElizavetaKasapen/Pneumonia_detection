96157f565db71824044d2afc03de27c0a2f7fdcf
ElizavetaKasapen <40471098+ElizavetaKasapen@users.noreply.github.com>
2021-05-30 12:14:10 +0300

    Add files via upload

--- data_processing.py
+++ data_processing.py
@@ -14,7 +14,7 @@ class data_preprocessing_for_net():
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
+        ]) 
         train_dataset = torchvision.datasets.ImageFolder(train_dir, data_to_tranform)
         val_dataset = torchvision.datasets.ImageFolder(val_dir, data_to_tranform)
         #droplast - drop last batch if it's not 23
@@ -24,15 +24,33 @@ class data_preprocessing_for_net():
            val_dataset, batch_size=batch_size, shuffle=False, drop_last = True)
         return train_dataloader, val_dataloader
 
-    def transform_image_to_predict(self, path = "Pneumonia_data/data/test/BAC_PNEUMONIA/person88_bacteria_439.jpeg"):
+    def transform_image_to_predict(self, image):
         my_transforms = transforms.Compose([transforms.Resize(255),#mb out this function
                                             transforms.CenterCrop(224),
                                             transforms.ToTensor(),
                                             transforms.Normalize(
                                                 [0.485, 0.456, 0.406],
                                                 [0.229, 0.224, 0.225])])
-        with open(path, 'rb') as f:
-            image_bytes = f.read()
-            image = Image.open(io.BytesIO(image_bytes))
-            image = image.convert("RGB")
+        
+       # image = Image.open(image)#comment it!!!!!!!!!!!!!!!!!!!!!!!!!
+        image = image.convert("RGB")
         return my_transforms(image).unsqueeze(0)
+    
+    def find_accuracy(self, test_dir = "Pneumonia_data/data/test"):
+        batch_size = 23
+        data_to_tranform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]) 
+        test_dataset = ImageFolderWithPaths(test_dir, data_to_tranform)
+        test_dataloader = torch.utils.data.DataLoader(
+            test_dataset, batch_size=batch_size, shuffle=True, drop_last = True)
+        return test_dataloader
+
+class ImageFolderWithPaths(torchvision.datasets.ImageFolder):
+    def __getitem__(self, index):
+        original_tuple = super(ImageFolderWithPaths, self).__getitem__(index)
+        path = self.imgs[index][0]
+        tuple_with_path = (original_tuple + (path,))
+        return tuple_with_path
